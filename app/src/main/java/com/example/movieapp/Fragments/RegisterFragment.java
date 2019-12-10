504533d5350a0a7b999201498d381271fbfbc1af
Adél Mészáros <adel.meszaros@yahoo.com>
2019-12-11 01:17:02 +0200

    Writing database helper

--- app/src/main/java/com/example/movieapp/Fragments/RegisterFragment.java
+++ app/src/main/java/com/example/movieapp/Fragments/RegisterFragment.java
@@ -1,7 +1,5 @@
 package com.example.movieapp.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,103 +7,59 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.movieapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RegisterFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RegisterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RegisterFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText et_email, et_username,et_password;
+    Button btn_register;
 
-    private OnFragmentInteractionListener mListener;
-
-    public RegisterFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_register, container,false);
+        et_username = v.findViewById(R.id.et_userName);
+        et_email = v.findViewById(R.id.et_email);
+        et_password = v.findViewById(R.id.et_password);
+        btn_register = v.findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = et_username.getText().toString();
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+                if(username.isEmpty()){
+                    et_username.setError("Please enter your username");
+                    et_username.requestFocus();
+                }
+                else if (email.isEmpty()){
+                    et_email.setError("Please enter your email");
+                    et_email.requestFocus();
+                }
+                else if(password.isEmpty()){
+                    et_password.setError("Please enter your password");
+                    et_password.requestFocus();
+                }
+                else if(username.isEmpty() && email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(getActivity(),"Field are empty", Toast.LENGTH_SHORT).show();
+                }
+                else if(!(username.isEmpty() && email.isEmpty() && password.isEmpty())){
+                    //adatbazisos cucok
+                }
+            }
+        });
+
+
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/com/example/movieapp/Models/User.java
+++ app/src/main/java/com/example/movieapp/Models/User.java
@@ -2,17 +2,6 @@ package com.example.movieapp.Models;
 
 public class User {
 
-    public static final String TABLE_NAME = "Users";
-    public static final String COLUMN_ID = "id";
-    public static final String COLUMN_NAME = "name";
-    public static final String COLUMN_EMAIL = "email";
-    public static final String COLUMN_PASSWORD = "passwrod";
-    public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
-            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + COLUMN_NAME + " TEXT, "
-            + COLUMN_EMAIL + " TEXT, "
-            +COLUMN_PASSWORD + " TEXT )";
-
     private int userId;
     private String userName;
     private String userEmail;
