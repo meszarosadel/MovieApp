2c8d7bfcbcb7956cda0019a7faf0f25891061f16
Adél Mészáros <adel.meszaros@yahoo.com>
2019-12-10 22:37:58 +0200

    Initialiyation

---
+++ app/src/main/java/com/example/movieapp/Models/User.java
@@ -0,0 +1,47 @@
+package com.example.movieapp.Models;
+
+public class User {
+    String userId;
+    String userName;
+    String userEmail;
+    String password;
+
+    public User(String userId, String userName, String userEmail, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
