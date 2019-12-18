cae6d5f92fd4db6d26b59ed2a3e65ce5bdb72f4f
Adél Mészáros <adel.meszaros@yahoo.com>
2019-12-18 22:35:11 +0200

    Implemetation of reset password layout

--- app/src/main/java/com/example/movieapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/movieapp/Fragments/ProfileFragment.java
@@ -16,6 +16,8 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
@@ -24,6 +26,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.movieapp.R;
 
@@ -41,15 +44,17 @@ public class ProfileFragment extends Fragment {
 
     ImageView img_v;
     Button btn_save, btn_choose_image;
+    TextView tv_password;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        img_v = (ImageView) view.findViewById(R.id.img_v);
-        btn_save = (Button) view.findViewById(R.id.btn_save);
-        btn_choose_image = (Button) view.findViewById(R.id.btn_choose_image);
+        img_v = view.findViewById(R.id.img_v);
+        btn_save = view.findViewById(R.id.btn_save);
+        btn_choose_image = view.findViewById(R.id.btn_choose_image);
+        tv_password = view.findViewById(R.id.tv_password);
 
         btn_choose_image.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +67,25 @@ public class ProfileFragment extends Fragment {
                 }else{
                     startGallery();
                 }
+            }
+        });
+
+        tv_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new ResetPasswordFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_id, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
         return view;
---
+++ app/src/main/java/com/example/movieapp/Fragments/ResetPasswordFragment.java
@@ -0,0 +1,26 @@
+package com.example.movieapp.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.movieapp.R;
+
+
+public class ResetPasswordFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_reset_password, container, false);
+        return view;
+    }
+
+}
